5f5e64cd02b25a77410e22345b0b47efb80982cd
Lucas Carvalho <14770259794@cefet-rj.br>
2023-06-12 11:52:07 -0300

    New code

--- src/main/java/me/lucthesloth/domhusks/EntityListener.java
+++ src/main/java/me/lucthesloth/domhusks/EntityListener.java
@@ -20,27 +20,8 @@ public class EntityListener implements Listener {
             !(event.getEntity() instanceof Monster) ||
             event.getLocation().getWorld().getEnvironment() != World.Environment.NORMAL) return;
 
-        oldCode(event);
-    }
-    public void oldCode(CreatureSpawnEvent event) {
-        //Old code
-        EntityType entityType = event.getEntityType();
         Location location = event.getLocation();
-        Biome biome = location.getBlock().getBiome();
-        double structureX = 0.0D;
-        double structureZ = 0.0D;
-        //Changed to structure search result to avoid using deprecated method
-        StructureSearchResult result = null;
-        //Removed specific entity type check in favor of checking only for monsters
-        //There were only 2 possible cases of an entity not being valid: Enderman and Drowneds, which
-        //Would not spawn inside a husk farm in any case
-        if (biome == Biome.DESERT)
-            result = location.getWorld().locateNearestStructure(location, StructureType.DESERT_PYRAMID, 25, false);
-        if (result != null) {
-            structureZ = result.getLocation().getZ();
-            structureX = result.getLocation().getX();
-        }
-        if (location.getY() >= 50.0D && location.getY() <= 80.0D && Math.abs(structureX - location.getX() + 11.0D) < 12.0D && Math.abs(structureZ - location.getZ() + 11.0D) < 12.0D) {
+        if (HuskPyramid.instance.pyramidBoundingBoxes.stream().anyMatch(box -> box.contains(location.toVector()))){
             location.getWorld().spawnEntity(location, EntityType.HUSK);
             event.setCancelled(true);
         }
--- src/main/java/me/lucthesloth/domhusks/HuskPyramid.java
+++ src/main/java/me/lucthesloth/domhusks/HuskPyramid.java
@@ -8,12 +8,12 @@ import java.util.*;
 
 public final class HuskPyramid extends JavaPlugin {
     public ArrayList<BoundingBox> pyramidBoundingBoxes = new ArrayList<>();
-    public boolean old = false;
     public static HuskPyramid instance;
     @Override
     public void onEnable() {
         saveDefaultConfig();
         // Plugin startup logic
+        populateList();
         Bukkit.getPluginManager().registerEvents(new EntityListener(), this);
         instance = this;
     }
@@ -23,5 +23,9 @@ public final class HuskPyramid extends JavaPlugin {
         // Plugin shutdown logic
     }
 
-
+    public void populateList() {
+        List<List<Integer>> list = (List<List<Integer>>) getConfig().getList("pyramids", Collections.emptyList());
+        for (List<Integer> location : list)
+            this.pyramidBoundingBoxes.add(new BoundingBox(location.get(0), 50.0D, location.get(1), location.get(0) + 21.0D, 80.0D, location.get(1) + 21.0D));
+    }
 }
