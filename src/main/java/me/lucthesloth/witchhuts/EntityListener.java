4c4ad95514db19d126e43218a001d892ba735c8d
Lucas Carvalho <14770259794@cefet-rj.br>
2023-06-19 14:49:46 -0300

    Optimization

--- src/main/java/me/lucthesloth/witchhuts/EntityListener.java
+++ src/main/java/me/lucthesloth/witchhuts/EntityListener.java
@@ -2,6 +2,7 @@ package me.lucthesloth.witchhuts;
 
 import org.bukkit.Location;
 import org.bukkit.World;
+import org.bukkit.block.Biome;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Monster;
 import org.bukkit.event.EventHandler;
@@ -15,9 +16,11 @@ public class EntityListener implements Listener {
     public void onCreatureSpawn(CreatureSpawnEvent event){
         if (event.getSpawnReason() != CreatureSpawnEvent.SpawnReason.NATURAL ||
             !(event.getEntity() instanceof Monster) ||
-            event.getLocation().getWorld().getEnvironment() != World.Environment.NORMAL) return;
+            event.getLocation().getWorld().getEnvironment() != World.Environment.NORMAL ||
+            event.getLocation().getBlock().getBiome() == Biome.SWAMP) return;
 
         Location location = event.getLocation();
+        if (location.getBlockY() < WitchHuts.instance.baseHeight - 1 || location.getBlockY() > WitchHuts.instance.baseHeight + WitchHuts.instance.ySize + 1) return;
         if (WitchHuts.instance.hutsBoundingBoxes.stream().anyMatch(box -> box.contains(location.toVector()))){
             location.getWorld().spawnEntity(location, EntityType.WITCH);
             event.setCancelled(true);
